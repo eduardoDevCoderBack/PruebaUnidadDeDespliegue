12de8939192c9565912ad6fd638f5d89380af22c
eduardoDevCoderBack <eduardoarmando.munoz@gmail.com>
2024-09-03 17:44:47 -0600

    se suben las entradas de trx

--- artifact/transactions/MZICT002-01-MX/src/main/java/com/bbva/mzic/AbstractMZICT00201MXTransaction.java
+++ artifact/transactions/MZICT002-01-MX/src/main/java/com/bbva/mzic/AbstractMZICT00201MXTransaction.java
@@ -1,6 +1,7 @@
 package com.bbva.mzic;
 
 import com.bbva.elara.transaction.AbstractTransaction;
+import com.bbva.mzic.dto.projecto.PRUEBA2;
 
 /**
  * In this class, the input and output data is defined automatically through the setters and getters.
@@ -10,4 +11,39 @@ public abstract class AbstractMZICT00201MXTransaction extends AbstractTransactio
 	public AbstractMZICT00201MXTransaction(){
 	}
 
+
+	/**
+	 * Return value for input parameter MZICC008
+	 */
+	protected PRUEBA2 getMzicc008(){
+		return (PRUEBA2)this.getParameter("MZICC008");
+	}
+
+	/**
+	 * Return value for input parameter Name
+	 */
+	protected String getName(){
+		return (String)this.getParameter("Name");
+	}
+
+	/**
+	 * Return value for input parameter Phone
+	 */
+	protected Long getPhone(){
+		return (Long)this.getParameter("Phone");
+	}
+
+	/**
+	 * Return value for input parameter LastName
+	 */
+	protected String getLastname(){
+		return (String)this.getParameter("LastName");
+	}
+
+	/**
+	 * Return value for input parameter email
+	 */
+	protected String getEmail(){
+		return (String)this.getParameter("email");
+	}
 }
